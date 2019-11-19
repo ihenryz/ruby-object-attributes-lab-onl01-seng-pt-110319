e59b21567240a0b9ad4398ef5a1a489c79262a96
Henry Phan <h.phan06@gmail.com>
2019-11-19 22:27:30 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,19 +1,2 @@
-class Person
-  
-  def name=(p_name)
-    @name = p_name
-  end
-  
-  def name
-    @name
-  end
-  
-  def job=(job)
-    @job = job
-  end
-  
-  def job
-    @job
-  end
-  
-end
+class Person 
+end 
