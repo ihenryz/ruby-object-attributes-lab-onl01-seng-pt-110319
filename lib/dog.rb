9a00ea3e8c3a943de6b4c82d28fc5f0a346c3748
Henry Phan <h.phan06@gmail.com>
2019-11-19 22:27:11 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,30 @@
-require 'pry'
-class Dog
-
-  def name=(dog_name)
-    @name = dog_name
-  end
-
-  def name
-    @name
-  end
-
-  def breed=(dog_breed)
-    @breed = dog_breed
-  end
-
-  def breed
-    @breed
-  end
-
-
+class Dog 
+  attr_accessor :name, :breed
+  attr_reader :breed
+  
 end
+
+fido = Dog.new()
+fido.name = "Fido"
+
+
+# class Dog
+
+#   def name=(dog_name)
+#     @name = dog_name
+#   end
+
+#   def name
+#     @name
+#   end
+
+#   def breed=(dog_breed)
+#     @breed = dog_breed
+#   end
+
+#   def breed
+#     @breed
+#   end
+
+
+# end
